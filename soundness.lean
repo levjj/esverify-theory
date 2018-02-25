1d833ec25809ae97f6bb6b7e45a9ba6860d814fa
Christopher Schuster <cs@livoris.net>
2018-02-24 23:26:19 -0800

    trivial precondition dominates

--- soundness.lean
+++ soundness.lean
@@ -2,19 +2,6 @@ import data.set
 
 import .syntax .notations .logic .evaluation .vcgen .progress .preservation
 
-lemma trivial_precondition:
-  ∀σ, σ ⊨ vc.implies (prop.instantiated_n (value.true ⋀ value.true)) (prop.instantiated_n (value.true)) :=
-  assume σ: env,
-  show σ ⊨ vc.implies (prop.instantiated_n (value.true ⋀ value.true)) (prop.instantiated_n (value.true)),
-  from valid_env.mpr (
-    assume _,
-    have h: σ ⊨ value.true, from valid_env.true,
-    have prop.erased (prop.term value.true) = vc.term value.true, by unfold prop.erased,
-    have σ ⊨ (prop.erased (value.true)), from this.symm ▸ h,
-    have σ ⊨ (prop.instantiated (value.true)), from valid_env.instantiated_of_erased this,
-    show σ ⊨ (prop.instantiated_n (value.true)), from valid_env.instantiated_n_of_instantiated this
-  )
-
 lemma trivial_freevars: FV (prop.term value.true ⋀ prop.term value.true) = FV (prop.term value.true) :=
   have h1: FV (prop.term value.true ⋀ prop.term value.true) = ∅, from set.eq_empty_of_forall_not_mem (
     assume x: var,
@@ -38,45 +25,80 @@ lemma trivial_freevars: FV (prop.term value.true ⋀ prop.term value.true) = FV 
   ),
   show FV (prop.term value.true ⋀ prop.term value.true) = FV (prop.term value.true), from eq.trans h1 h2.symm
 
+lemma trivial_precondition:
+  ∀σ, σ ⊨ vc.implies (prop.instantiated_n (value.true ⋀ value.true)) (prop.instantiated_n (value.true)) :=
+  assume σ: env,
+  show σ ⊨ vc.implies (prop.instantiated_n (value.true ⋀ value.true)) (prop.instantiated_n (value.true)),
+  from valid_env.mpr (
+    assume _,
+    have h: σ ⊨ value.true, from valid_env.true,
+    have prop.erased (prop.term value.true) = vc.term value.true, by unfold prop.erased,
+    have σ ⊨ (prop.erased (value.true)), from this.symm ▸ h,
+    have σ ⊨ (prop.instantiated (value.true)), from valid_env.instantiated_of_erased this,
+    show σ ⊨ (prop.instantiated_n (value.true)), from valid_env.instantiated_n_of_instantiated this
+  )
+
 lemma trivial_calls: calls (prop.term value.true ⋀ prop.term value.true) = calls (prop.term value.true) :=
   have h1: calls (prop.term value.true ⋀ prop.term value.true) = ∅, from set.eq_empty_of_forall_not_mem (
     assume c: calltrigger,
     assume : c ∈ calls (prop.term value.true ⋀ prop.term value.true),
-    have c ∈ calls (prop.term value.true) ∨ c ∈ calls (prop.term value.true), from has_call.and.inv this,
+    have c ∈ calls (prop.term value.true) ∨ c ∈ calls (prop.term value.true), from prop.has_call.and.inv this,
     or.elim this (
       assume : c ∈ calls (prop.term value.true),
-      show «false», from has_call.term.inv this
+      show «false», from prop.has_call.term.inv this
     ) (
       assume : c ∈ calls (prop.term value.true),
-      show «false», from has_call.term.inv this
+      show «false», from prop.has_call.term.inv this
     )
   ),
   have h2: calls (prop.term value.true) = ∅, from set.eq_empty_of_forall_not_mem (
     assume c: calltrigger,
     assume : c ∈ calls (prop.term value.true),
-    show «false», from has_call.term.inv this
+    show «false», from prop.has_call.term.inv this
   ),
   show calls (prop.term value.true ⋀ prop.term value.true) = calls (prop.term value.true), from eq.trans h1 h2.symm
 
-lemma trivial_quantifiers: quantifiers (prop.term value.true ⋀ prop.term value.true) = quantifiers (prop.term value.true) :=
-  have h1: quantifiers (prop.term value.true ⋀ prop.term value.true) = ∅, from set.eq_empty_of_forall_not_mem (
-    assume q: callquantifier,
-    assume : q ∈ quantifiers (prop.term value.true ⋀ prop.term value.true),
-    have q ∈ quantifiers (prop.term value.true) ∨ q ∈ quantifiers (prop.term value.true), from has_quantifier.and.inv this,
-    or.elim this (
-      assume : q ∈ quantifiers (prop.term value.true),
-      show «false», from has_quantifier.term.inv this
-    ) (
-      assume : q ∈ quantifiers (prop.term value.true),
-      show «false», from has_quantifier.term.inv this
-    )
+lemma trivial_dominates {σ: env}: dominates σ (prop.term value.true ⋀ prop.term value.true) (prop.term value.true) :=
+  let P:prop := prop.term value.true ⋀ prop.term value.true in
+  let P':prop := prop.term value.true in
+
+  have h_impl: σ ⊨ vc.implies P.instantiated_n P'.instantiated_n, from trivial_precondition σ,
+
+  have calls_env σ P = (calltrigger.subst σ) '' calls (prop.term value.true ⋀ prop.term value.true), from rfl,
+  have h1: calls_env σ P = (calltrigger.subst σ) '' calls (prop.term value.true), from trivial_calls ▸ this,
+  have h2: calls_env σ P' = (calltrigger.subst σ) '' calls (prop.term value.true), from rfl,
+  have h_calls: calls_env σ P = calls_env σ P', from eq.trans h1 h2.symm,
+
+  have h_quantifiers:
+    (∀(t': term) (x: var) (Q': prop) (h: callquantifier.mk t' x Q' ∈ quantifiers P'),
+                          have Q'.size < P'.size, from quantifiers_smaller_than_prop.left h,
+    ∃(t: term) (Q: prop), callquantifier.mk t x Q ∈ quantifiers P ∧
+                          (∀v: value, dominates' Q' Q (σ[x↦v]))), from (
+    assume (t': term) (x:var) (Q': prop),
+    assume h3: callquantifier.mk t' x Q' ∈ quantifiers P',
+    have h4: callquantifier.mk t' x Q' ∉ quantifiers P', from prop.has_quantifier.term.inv,
+    absurd h3 h4
   ),
-  have h2: quantifiers (prop.term value.true) = ∅, from set.eq_empty_of_forall_not_mem (
-    assume q: callquantifier,
-    assume : q ∈ quantifiers (prop.term value.true),
-    show «false», from has_quantifier.term.inv this
-  ),
-  show quantifiers (prop.term value.true ⋀ prop.term value.true) = quantifiers (prop.term value.true), from eq.trans h1 h2.symm
+
+  have h3: 
+    ((σ ⊨ vc.implies P.instantiated_n P'.instantiated_n) ∧
+    (calls_env σ P = calls_env σ P') ∧
+    (∀(t': term) (x: var) (Q': prop) (h: callquantifier.mk t' x Q' ∈ quantifiers P'),
+                          have Q'.size < P'.size, from quantifiers_smaller_than_prop.left h,
+    ∃(t: term) (Q: prop), callquantifier.mk t x Q ∈ quantifiers P ∧
+                          (∀v: value, dominates' Q' Q (σ[x↦v])))),
+  from ⟨h_impl, ⟨h_calls, h_quantifiers⟩⟩,
+  have
+    dominates' P' P σ = (
+    ((σ ⊨ vc.implies P.instantiated_n P'.instantiated_n) ∧
+    (calls_env σ P = calls_env σ P') ∧
+    (∀(t': term) (x: var) (Q': prop) (h: callquantifier.mk t' x Q' ∈ quantifiers P'),
+                          have Q'.size < P'.size, from quantifiers_smaller_than_prop.left h,
+    ∃(t: term) (Q: prop), callquantifier.mk t x Q ∈ quantifiers P ∧
+                          (∀v: value, dominates' Q' Q (σ[x↦v]))))),
+  by unfold1 dominates',
+  have dominates' P' P σ, from this.symm ▸ h3,
+  show dominates σ P P', from this
 
 lemma soundness {s s': stack}: (s ⟶* s') → ∀H, (H ⊢ₛ s) → (is_value s' ∨ ∃h s'', s' ⟶ h, s'') :=
   assume steps_to_s': s ⟶* s',
@@ -101,7 +123,7 @@ theorem soundness_source_programs {e: exp} {s: stack} {Q: propctx}:
   assume initial_verified: value.true ⊢ e: Q,
   assume steps_to_s: (env.empty, e) ⟶* s,
   have (prop.term value.true ⋀ prop.term value.true) ⊢ e: Q,
-  from strengthen_pre_exp initial_verified (prop.term value.true ⋀ prop.term value.true)
-       trivial_freevars trivial_calls trivial_quantifiers trivial_precondition,
+  from strengthen_exp initial_verified (prop.term value.true ⋀ prop.term value.true)
+       trivial_freevars (λσ, trivial_dominates),
   have list.nil ⊢ₛ (env.empty, e), from stack.vcgen.top env.vcgen.empty this,
   show is_value s ∨ ∃h s', s ⟶ h, s', from soundness steps_to_s list.nil this
